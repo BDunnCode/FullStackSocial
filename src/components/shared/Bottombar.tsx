63846914e5347caeb20d1eac23469dcc248cb50c
BDunnCode <BDunnCode@github.com>
2024-01-17 18:53:01 -0600

    Working post forms and a FileUploader

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,3 +1,4 @@
+import { bottombarLinks } from '@/constants';
 import { Link, useLocation } from 'react-router-dom'
  
 const Bottombar = () => {
@@ -6,26 +7,27 @@ const Bottombar = () => {
   return (
 
     <section className="bottom-bar">
-       {sidebarLinks.map((link: INavLink) => {
+       {bottombarLinks.map((link) => {
             const isActive = pathname === link.route;
             return (
-              <li key={link.label} className=
-              {`leftsidebar-link group ${
-                isActive && 'bg-primary-500' 
-              }`}>
-                <NavLink
+                <Link
                   to={link.route}
-                  className="flex gap-4 items-center p-4"
+                  key={link.label} 
+                  className=
+                  {`${
+                    isActive && 'bg-primary-500 rounded-[10px]' 
+                  } flex-center flex-col gap-1 p-2 transition`}                
                 >
                   <img
                     src={link.imgURL}
                     alt={link.label}
+                    width={16}
+                    height={16}
                     className=
-                    {`group-hover:invert-white ${isActive && 'invert-white'}`}
+                    {`${isActive && 'invert-white'}`}
                   />
-                  {link.label}
-                </NavLink>
-              </li>
+                  <p className="tiny-medium text-light-2">{link.label}</p>
+                </Link>
             )
           })}
     </section>
