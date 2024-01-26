43883302b6d88eb84463eca8384b7078dcb046a0
BDunnCode <BDunnCode@github.com>
2024-01-25 19:38:32 -0600

    nearly done now

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -11,7 +11,7 @@ import { Textarea } from "../ui/textarea"
 import FileUploader from "../shared/FileUploader"
 import { PostValidation } from "@/lib/validation"
 import { Models } from "appwrite"
-import { useCreatePost } from "@/lib/react-query/queriesAndMutations"
+import { useCreatePost, useUpdatePost } from "@/lib/react-query/queriesAndMutations"
 import { UserRoundIcon } from "lucide-react"
 import { useUserContext } from "@/context/AuthContext"
 import { useToast } from "../ui/use-toast"
@@ -24,6 +24,10 @@ type PostFormProps = {
 const PostForm = ({ post, action }: PostFormProps) => {
     const { mutateAsync: createPost, isPending: isLoadingCreate } =
     useCreatePost();
+    const { mutateAsync: updatePost, isPending: isLoadingUpdate } =
+    useUpdatePost();
+  
+
     const { user } = useUserContext();
     const { toast } = useToast();
     const navigate = useNavigate()
@@ -43,6 +47,21 @@ const PostForm = ({ post, action }: PostFormProps) => {
     // 2. Define a submit handler. 
     // Getting an error saying useCreatePost is an ambiguous indirect export, breaking the page
     async function onSubmit(values: z.infer<typeof PostValidation>) {
+      if(post && action === 'Update') {
+        const updatedPost = await updatePost({
+          ...values,
+          postId: post.$id,
+          imageId: post?.imageId,
+          imageUrl: post?.imageUrl
+        })
+
+        if(!updatedPost) {
+          toast({ title: 'Please try again'})
+        }
+
+        return navigate(`/posts/{post.$id}`);
+      }
+      
       const newPost = await createPost({
         ...values,
         userId: user.id,
@@ -131,7 +150,11 @@ const PostForm = ({ post, action }: PostFormProps) => {
           <Button 
           type="submit"
           className="shad-button-primary whitespace-nowrap"
-          >Submit</Button>
+          disabled={isLoadingCreate || isLoadingUpdate}
+          >
+            {isLoadingCreate || isLoadingUpdate && 'Loading...'}
+            {action} Post
+          </Button>
         </div>
       </form>
     </Form>  )
