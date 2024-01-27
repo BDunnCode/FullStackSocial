cadc2cf16c21dfdc465c938a5bc86f5384f89edc
BDunnCode <BDunnCode@github.com>
2024-01-27 17:12:31 -0600

    finish main portion. ready for debugs, edits, and additions

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -174,7 +174,7 @@ export const useUpdatePost = (postId: string) => {
 
   export const useSearchPosts = (searchTerm: string) => {
     return useQuery({
-      queryKey: [QUERY_KEYS.SEARCH_POSTS],
+      queryKey: [QUERY_KEYS.SEARCH_POSTS, searchTerm],
       queryFn: () => searchPosts(searchTerm),
       enabled: !!searchTerm
     })
