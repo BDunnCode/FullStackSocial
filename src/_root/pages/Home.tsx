71b3f5087c9fa9ef96018f762125ecd681cb1c5e
BDunnCode <BDunnCode@github.com>
2024-02-23 23:00:30 -0600

    infinite scroll appears to be stable and fully functional

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,31 +1,38 @@
 import Loader from "@/components/shared/Loader";
 import PostCard from "@/components/shared/PostCard";
-import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
+import { useGetPosts } from "@/lib/react-query/queriesAndMutations";
 import { Models } from "appwrite";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 const Home = () => {
-  const { data: posts, isPending: isPostLoading, isError:
-  isErrorPosts } = useGetRecentPosts();
+  const { data: posts, fetchNextPage, hasNextPage, isFetching } = useGetPosts();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetching) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetching]);
 
   return (
     <div className="flex flex-1">
       <div className="home-container">
         <div className="home-posts">
-          <h2 className="h3-bold md:h2-bold text-left w-full">Home 
-          Feed</h2>
-          {isPostLoading && !posts ? (
-            <Loader />
-          ) : (
-            <ul className="flex flex-col flex-1 gap-9 w-full">
-              {posts?.documents.map((post: Models.Document) => (
-                <PostCard post={post} key={post.caption}/>
-              ))}
-            </ul>
-          )}
+          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          <ul className="flex flex-col flex-1 gap-9 w-full">
+            {(posts?.pages || []).map((page, pageIndex) =>
+              page?.documents.map((post: Models.Document) => (
+                <PostCard post={post} key={`page-${pageIndex}-${post.id}`} />
+              ))
+            )}
+          </ul>
+          {isFetching && <Loader />}
         </div>
+        <div ref={ref}></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
