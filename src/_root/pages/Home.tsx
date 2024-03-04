d5b1bc45b2282804c714175c5296bf660318cbb7
BDunnCode <BDunnCode@github.com>
2024-03-04 16:24:08 -0600

    add error page, css cleanup

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,6 +1,7 @@
 import CreatorCard from "@/components/shared/CreatorCard";
 import Loader from "@/components/shared/Loader";
 import PostCard from "@/components/shared/PostCard";
+
 import { useGetPosts, useGetTopUsers } from "@/lib/react-query/queriesAndMutations";
 
 
@@ -9,18 +10,28 @@ import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 
 const Home = () => {
-  const { data: posts, fetchNextPage, hasNextPage, isFetching } = useGetPosts();
+  const { data: posts, fetchNextPage, hasNextPage, isFetching, isLoading: isPostLoading, isError: isErrorPosts } = useGetPosts();
   const { ref, inView } = useInView();
-  const {data: topUsers, isLoading, isError} = useGetTopUsers();
-
-  console.log(topUsers)
-
+  const { data: topUsers, isLoading: isTopUsersLoading, isError: isErrorTopUsers } = useGetTopUsers();
 
   useEffect(() => {
     if (inView && hasNextPage && !isFetching) {
       fetchNextPage();
     }
   }, [inView, hasNextPage, isFetching]);
+
+  if (isErrorPosts || isErrorTopUsers) {
+    return (
+      <div className="flex flex-1">
+        <div className="home-container">
+          <p className="body-medium text-light-1">Sorry, something went wrong.</p>
+        </div>
+        <div className="top-creators-sidebar">
+          <p className="body-medium text-light-1">Sorry, something went wrong.</p>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div className="flex flex-1">
@@ -39,7 +50,7 @@ const Home = () => {
         <div ref={ref}></div>
       </div>
 
-      <div className="hidden xl:flex flex-col py-5 px-4 bg-dark-2 overflow-y-scroll">
+      <div className="top-creators-sidebar">
         <h3 className="h3-bold">Top Creators</h3>
         <div className="grid grid-cols-2 grid-rows-4 gap-4 py-4">
           {topUsers?.documents.map((user) => (
