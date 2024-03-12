2acfe8811c299fd7e16042df2c3d1b7cf5770639
BDunnCode <BDunnCode@github.com>
2024-03-11 22:50:06 -0500

    fixed wrong text inside log in button

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -101,7 +101,7 @@ const SigninForm = () => {
 
           <p className="text-small-regular text-light-2 text-center mt-2">
               Don't have an account?
-            <Link to="/sign-up" className="text-primary-500 text-small-semibold ml-1">Sign up</Link>
+            <Link to="/sign-up" className="text-primary-500 text-small-semibold ml-1">Log in</Link>
           </p>
         </form>
       </div>
