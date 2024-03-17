43de4bbf75dd1ea21a9f2170f6b946c5a8d6dbe2
BDunnCode <BDunnCode@github.com>
2024-03-17 17:17:43 -0500

    building out back end logic, zod profile validation, updateuser function

--- src/_root/pages/UpdateProfile.tsx
+++ src/_root/pages/UpdateProfile.tsx
@@ -1,4 +1,40 @@
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { 
+  Form, 
+  FormControl, 
+  FormField, 
+  FormItem, 
+  FormLabel, 
+  FormMessage,
+} from "@/components/ui/form"
+
+import { ProfileValidation } from "@/lib/validation";
+import { useUserContext } from "@/context/AuthContext";
+
+
 const UpdateProfile = () => {
+  
+
+
+  const { user, setUser } = useUserContext();
+  
+  const form = useForm<z.infer<typeof ProfileValidation>>({
+    resolver: zodResolver(ProfileValidation),
+    defaultValues: {
+      file: [],
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      bio: user.bio || "",
+    },
+  });
+
+  // Queries
+
+  
   return (
     <div>UpdateProfile</div>
   )
