787ec3a53602913efddae93e90c3b7267a001bc2
BDunnCode <BDunnCode@github.com>
2024-03-24 17:37:55 -0500

    put a max-width limit on on related posts h3

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -3,21 +3,47 @@ import { Loader } from "@/components/shared/Loader";
 import { PostStats } from "@/components/shared/PostStats";
 import { Button } from "@/components/ui/button";
 import { useUserContext } from "@/context/AuthContext";
-import { useGetPostById, useGetUserPostsByUserId } from "@/lib/react-query/queriesAndMutations";
+import { useDeletePost, useGetPostById, useGetUserPostsByUserId } from "@/lib/react-query/queriesAndMutations";
 import { formatDate } from "@/lib/utils";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const PostDetails = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
-  const { data: post, isPending } = useGetPostById(id || '');
   const { user } = useUserContext();
 
-  const {data: userPosts } = useGetUserPostsByUserId(user.id);
+  const { data: post, isPending } = useGetPostById(id || '');
+  const { data: userPosts } = useGetUserPostsByUserId(user.id);
+  const { mutate: deletePost } = useDeletePost();
 
-  const handleDeletePost = () => {};
+  const relatedPosts = userPosts?.documents.filter(( userPost ) => {
+    userPost.$id !== id;
+  })
+
+  const handleDeletePost = () => {
+    if (!id) return;
+
+    deletePost({ postId: id, imageId: post?.imageId });
+    navigate(-1);
+  };
   
   return (
     <div className="post_details-container">
+      <div className="hidden md:flex max-w-5xl w-full">
+        <Button
+          onClick={() => navigate(-1)}
+          variant="ghost"
+          className="shad-button_ghost">
+          <img
+            src={"/assets/icons/back.svg"}
+            alt="back"
+            width={24}
+            height={24}
+          />
+          <p className="small-medium lg:base-medium">Back</p>
+        </Button>
+       </div>
+
       {isPending ? <Loader /> : (
         <div className="post_details-card">
           <img 
@@ -94,11 +120,11 @@ const PostDetails = () => {
       )}
       <hr className="border w-full border-dark-4/80" />
       
-      <h3 className="body-bold md:h3-bold w-full my-10">More Related Posts</h3>
+      <h3 className="body-bold md:h3-bold w-full max-w-5xl my-10">Related Posts</h3>
       <div>
         {
-          userPosts && userPosts.documents && 
-          <GridPostList posts={userPosts.documents} showUser={false} showStats={false} />
+          relatedPosts &&
+          <GridPostList posts={relatedPosts} showUser={false} showStats={false} />
         }
       </div>
     </div>
