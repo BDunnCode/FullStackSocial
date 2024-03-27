20557c0ed0fb22313ceb73ae20ed27739c7b5dc4
BDunnCode <BDunnCode@github.com>
2024-03-27 18:00:14 -0500

    fix button display and sizing issues.

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -83,7 +83,7 @@ const Profile = () => {
                 </p>
               </Link>
             </div>
-            <div className={`user.id === id && "hidden"`}>
+            <div className={`${user.id === id && "hidden"}`}>
               <Button type="button" className="shad-button_primary px-8">
                 Follow
               </Button>
